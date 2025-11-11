36d29a369c27ef479c1492c1c95109930c908da5
Gabriel Granado <46542929+Thilanguis@users.noreply.github.com>
2025-11-11 02:02:43 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Mude para a próxima versão (ex: v26)
-const CACHE_NAME = 'simulador-cache-7';
+const CACHE_NAME = 'simulador-cache-8';
 const urlsToCache = [
   '.',
   'index.html', // --- CSS ---
